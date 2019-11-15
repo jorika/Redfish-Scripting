9b5a97e73d7e22e7a08dfcf6fe928efadd733509
Evgeny ISAEV <se.eugene@gmail.com>
2019-11-15 06:44:04 -0500

    Update Redfish-Factory-Restore.py

--- Redfish-Factory-Restore.py
+++ Redfish-Factory-Restore.py
@@ -17,7 +17,7 @@ userName = raw_input ("Please enter the Username: [by default Administrator] ")
 userPassword = raw_input ("Please enter the Password: [by default superuser] ")
 
 url = 'https://%s/redfish/v1/Managers/Self/Actions/RedfishDBReset' % (userIP)
-payload = {'ResetType': 'ResetAll'}
+payload = {'FactoryResetType': 'ResetAll'}
 headers = {'content-type': 'application/json'}
 
 response = requests.post(url, data=json.dumps(payload), headers=headers,verify=False,auth=(userName,userPassword))
